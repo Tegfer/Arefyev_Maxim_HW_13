bc55d020123177c433aecd8c5c9e67fa3b104760
Arefyev Maksim <tegferot@gmail.com>
2024-01-28 17:32:34 +0500

    Add feature to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,16 @@
 def capitalize_text():
+    """
+    This function make all letters of words capital
+    """
     string = input()
     string_upper = string.upper()
-    print(string_upper)
     return string_upper
-  
+
+
+def capital_first_characters():
+    """
+    This function make first letters of words capital
+    """
+    string = input()
+    string_capitalize = string.capitalize()
+    return string_capitalize
